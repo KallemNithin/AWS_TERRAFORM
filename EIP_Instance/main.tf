2c2b5bbc4c331bd440ed377be3a921ea5e662eb1
KallemNithin <kallemithin123@gmail.com>
2026-09-13 14:00:05 +0000

    EIP assigned to instance

---
+++ EIP_Instance/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0354c98ae10b02961"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "TerraformExample"
+  }
+}
+
+resource "aws_eip" "web_eip" {
+  instance = aws_instance.web.id
+  tags = {
+    Name = "Web_EIP"
+  }
+}
